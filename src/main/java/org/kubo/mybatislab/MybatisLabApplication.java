67ed3a72ffe54510d35c0d592f3d1af8fb203f45
zxj <775718540@qq.com>
2025-08-11 16:40:55 +0800

    first commit

---
+++ src/main/java/org/kubo/mybatislab/MybatisLabApplication.java
@@ -0,0 +1,13 @@
+package org.kubo.mybatislab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisLabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/kubo/mybatislab/MybatisLabApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kubo.mybatislab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
